7221feacc3b6c307a2f638054e5e577e64849c7b
YeBaited <jamespueblas@gmail.com>
2024-11-11 11:06:07 +0800

    Removed placeholder codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+  return (  
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-blue-400 flex h-[3rem]">
+        <div className="flex my-auto ml-1">
+          <h1 className="ml-1 text-2xl text-white">FEAPITSAT</h1>
+        </div>
+
+
+        <div className="ml-auto my-auto flex text-2xl">
+          <h1 className="mx-2 font-light text-white">Login</h1>
+          <h1 className="mx-2 font-light text-white">Enroll</h1>
+          <h1 className="mx-2 font-light text-white">Research</h1>
+          
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <img src="SchoolCover.jpg" className="justify-self-center object-contain" />
+
+      <div className="justify-self-center mb-5 flex">
+        <div className="mx-auto bg-red-300">
+          <h1>test</h1>
+        </div>
+        <div className="mx-auto">
+          <h1>test</h1>
+        </div>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="border border-black mx-auto w-[70rem]">
+        <h1 className="text-4xl text-center font-bold text-blue-500">Activities</h1>
+
+        <div className="flex justify-center p-5">
+
+          <div className="border border-black w-[20rem] p-5 mx-5">
+            <h1 className="text-center font-bold text-blue-500 text-lg">Mobile Legend!</h1>
+            <img src="Extracullicular/MobileLegend.jpg" alt="" />
+          </div>
+
+          <div className="border border-black w-[20rem] p-5 mx-5">
+            <h1 className="text-center font-bold text-blue-500 text-lg">Mobile Legend!</h1>
+            <img src="Extracullicular/MobileLegend.jpg" alt="" />
+          </div>
+
+          <div className="border border-black w-[20rem] p-5 mx-5">
+            <h1 className="text-center font-bold text-blue-500 text-lg">Mobile Legend!</h1>
+            <img src="Extracullicular/MobileLegend.jpg" alt="" />
+          </div>
+
+          
+
+        </div>
+      </div>
     </>
   )
 }
