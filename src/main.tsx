be6cbdf8eccb5efab9ba7def4b6948966b2b27bb
YeBaited <jamespueblas@gmail.com>
2024-11-11 19:20:10 +0800

    This is where I moved it

--- src/main.tsx
+++ src/main.tsx
@@ -28,6 +28,20 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <div className="bg-blue-400 flex h-[3rem]">
+        <div className="flex my-auto ml-1">
+          <h1 className="ml-1 text-2xl text-white">FEAPITSAT (THIS IS IN FUCKING PROGRESS)</h1>
+        </div>
+
+
+        <div className="ml-auto my-auto flex text-2xl">
+          <a href="/" className="mx-2 font-light text-white">Home</a>
+          <a href="/Login" className="mx-2 font-light text-white">Login</a>
+          <a href="/Enroll" className="mx-2 font-light text-white">Enroll</a>
+          
+
+        </div>
+      </div>
     <RouterProvider router={router}/>
   </StrictMode>,
 )
