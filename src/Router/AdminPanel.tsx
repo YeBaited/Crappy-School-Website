0b0ba9544f0b853fee7da8f5a4112ff6bda5ffca
YeBaited <jamespueblas@gmail.com>
2024-11-12 18:01:00 +0800

    Added some more placeholder info

--- src/Router/AdminPanel.tsx
+++ src/Router/AdminPanel.tsx
@@ -19,7 +19,89 @@ function AdminPanel(){
                 </div>
 
                 <div className="h-[30rem] w-[40rem] bg-blue-500 mx-1">
-                <h1 className="text-white text-xl font-bold text-center">Info</h1>
+                    <h1 className="text-white text-xl font-bold text-center">Info</h1>
+                    <div className="flex-row justify-center">
+
+
+                        <div className="flex justify-center">
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-right text-xl font-bold">Name:</h1>
+                            </div>  
+
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-left text-xl">Unk U. Unk</h1>
+                            </div>  
+
+                        </div>
+
+                        <div className="flex justify-center">
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-right text-xl font-bold">Age:</h1>
+                            </div>  
+
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-left text-xl">0</h1>
+                            </div>  
+
+                        </div>
+
+                        <div className="flex justify-center">
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-right text-xl font-bold">LRN:</h1>
+                            </div>  
+
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-left text-xl">0123456789</h1>
+                            </div>  
+
+                        </div>
+
+                        <div className="flex justify-center">
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-right text-xl font-bold">Previous School:</h1>
+                            </div>  
+
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-left text-xl">Unk High School</h1>
+                            </div>  
+
+                        </div>
+
+                        <div className="flex justify-center">
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-right text-xl font-bold">School Type:</h1>
+                            </div>  
+
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-left text-xl">Private Or Public</h1>
+                            </div>  
+
+                        </div>
+
+                        <div className="flex justify-center">
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-right text-xl font-bold">Course:</h1>
+                            </div>  
+
+                            <div className="p-1">
+                                <h1 className="w-[10rem] text-white text-left text-xl">ICT</h1>
+                            </div>  
+
+                        </div>
+
+                        <div className="flex justify-center mt-5">
+                              
+                            <button className="bg-green-500 mx-2 text-xl font-bold px-5 text-red-500">Approve</button>
+                            <button className="bg-red-500 mx-2 text-xl font-bold px-5 text-green-500">Decline</button>
+                        </div>
+
+
+
+
+                         
+                        <h1 className="text-xl text-white mt-2 ml-1">Status: Not yet responded!</h1>
+
+                    </div>
                 </div>
 
             </div>
