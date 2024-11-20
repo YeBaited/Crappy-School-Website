9d75d46509c68d488f45012bab2a457a1e964dbf
YeBaited <jamespueblas@gmail.com>
2024-11-20 14:11:58 +0800

    Finalized the functions

--- src/Router/Login.tsx
+++ src/Router/Login.tsx
@@ -1,24 +1,35 @@
 function tryCreate(){
-    var TestSend = {
-        "username" : "Bossing",
-        "password" : "Boss"
+    const username = document.querySelector("#username") as HTMLInputElement
+    const password = document.querySelector("#password") as HTMLInputElement
+
+    var data = {
+        "username" : username.value,
+        "password" : password.value
     }
+
+    
+
+    if (data.username == ""){
+        console.log("no username!")
+        return
+    }
+
 
     var ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function(){
-
+        console.log(this.response)
     }
 
     ajax.open("POST", "http://localhost:82/php/CreateAccount.php")
-    ajax.send(JSON.stringify(TestSend))
+    ajax.send(JSON.stringify(data))
 }
 
 function Login(){
     return (
         <>
             <h1>Login</h1>
-            <input className="border border-black" placeholder="Username" type="text" />
-            <input className="border border-black" placeholder="Password" type="text" />
+            <input className="border border-black" placeholder="Username" id="username" type="text" />
+            <input className="border border-black" placeholder="Password" id="password" type="text" />
             <button onClick={tryCreate}>Create</button>
         
         </>
