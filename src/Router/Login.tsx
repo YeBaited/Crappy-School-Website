b09f45fc09b7eec333bb5bdcdc3a6d3b1e07517c
YeBaited <jamespueblas@gmail.com>
2024-11-20 13:46:19 +0800

    Login tsx, prototype done

--- src/Router/Login.tsx
+++ src/Router/Login.tsx
@@ -1,7 +1,27 @@
+function tryCreate(){
+    var TestSend = {
+        "username" : "Bossing",
+        "password" : "Boss"
+    }
+
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function(){
+
+    }
+
+    ajax.open("POST", "http://localhost:82/php/CreateAccount.php")
+    ajax.send(JSON.stringify(TestSend))
+}
 
 function Login(){
     return (
-        <h1>Login</h1>
+        <>
+            <h1>Login</h1>
+            <input className="border border-black" placeholder="Username" type="text" />
+            <input className="border border-black" placeholder="Password" type="text" />
+            <button onClick={tryCreate}>Create</button>
+        
+        </>
     )
 }
 
