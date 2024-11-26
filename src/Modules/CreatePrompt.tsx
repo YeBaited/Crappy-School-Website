acd5604ac4b2b75b237b074e7baff9b6d561ccff
YeBaited <jamespueblas@gmail.com>
2024-11-26 20:39:23 +0800

    Interfacing Prop Done

--- src/Modules/CreatePrompt.tsx
+++ src/Modules/CreatePrompt.tsx
@@ -1,5 +1,26 @@
-function CreatePrompt(){
+interface prop {
+    "title" : string,
+    "description" : string,
+    "Buttons" : string,
+}
 
+function CreatePrompt({title, description, Buttons} : prop){
+    console.log(Buttons)
+    function AddButtons(){
+        if (Buttons == "YesNo"){
+            return (
+                <>
+                    <button className="w-[20%] bg-slate-400">Yes 1</button>
+                    <button className="w-[20%] bg-slate-400">No 2</button>
+                </>
+            )
+        }
+
+
+        return (
+            <h1>NO BUTTON</h1>
+        )
+    }
     return (
         <div>
             <div className="fixed top-0 bg-blue-950 h-full translate-y-[4.5%] w-full opacity-40">
@@ -7,15 +28,14 @@ function CreatePrompt(){
             </div>
             
             <div className="fixed w-2/6 h-2/6 bg-blue-500 translate-x-full translate-y-[20vh] rounded-sm">
-                <h1 className="text-center text-xl font-bold text-white">PROMPT NAME</h1>
+                <h1 className="text-center text-xl font-bold text-white">{title}</h1>
 
                 <div className="flex justify-center h-[80%]">
-                    <p className="text-justify text-white">TEXT HERE TEXT HERE TEXT HERE</p>
+                    <p className="text-justify text-white">{description}</p>
                 </div>
 
                 <div className="flex justify-evenly bg-red-500 opacity-50">
-                    <button className="w-[20%] bg-slate-400">OPTION 1</button>
-                    <button className="w-[20%] bg-slate-400">OPTION 2</button>
+                    {AddButtons()}
                 </div>
 
             </div>
